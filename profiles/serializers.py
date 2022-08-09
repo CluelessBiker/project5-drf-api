830dc7224b9e025b1b4c163b44e6cf37db648dbb
Lauren-Nicole <popich.red@gmail.com>
2022-08-09 11:27:32 +0000

    Create serializers for Profiles.

---
+++ profiles/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from .models import Profile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    """
+    Profile serializer.
+    Providing readability to profile data in API.
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        """
+        Associates User to User Profile.
+        """
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        """
+        Lists of Profile model fields to display.
+        """
+        model = Profile
+        fields = [
+            'id',
+            'owner',
+            'created_on',
+            'modified_on',
+            'description',
+            'image',
+            'first_name',
+            'last_name',
+            'username',
+            'country',
+            'is_owner',
+        ]
