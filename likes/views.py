27b0103b8e42745b07cb70d479d1e9afe0f44417
Lauren-Nicole <popich.red@gmail.com>
2022-08-09 13:39:49 +0000

    Create views for Likes app. Ability to like/unlike a post.

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from p5_drf_api.permissions import IsOwnerOrReadOnly
+from likes.models import Like
+from likes.serializers import LikeSerializer
 
-# Create your views here.
+
+# Class provided by DRF-API walkthrough.
+class LikeList(generics.ListCreateAPIView):
+    """
+    Provide logged in users the ability
+    to like/unlike a post.
+    """
+    permissions_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serilaizer_class = LikeSerializer
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        """
+        If user is authenticated, like/dislike
+        will be saved to database
+        """
+        serializer.save(owner=self.request.user)
+
+
+# Class provided by DRF-API walkthrough.
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    The ability to create/delete
+    a like.
+    """
+    permissions_classes = [IsOwnerOrReadOnly]
+    serilaizer_class = LikeSerializer
+    queryset = Like.objects.all()
