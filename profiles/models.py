f0d3d3eab830ba9e1608cfc5c64e98e85470cf5f
Lauren-Nicole <popich.red@gmail.com>
2022-08-09 09:58:26 +0000

    Create model for user profiles.

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Class provided by DRF-API walkthrough.
+# Modifications have been made.
+class Profile(models.Model):
+    """
+    Profile model for database.
+    """
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/',
+        default='../android-chrome-512x512_ziips8',
+    )
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    username = models.CharField(max_length=50, blank=True)
+    country = models.CharField(max_length=50, blank=True)
+
+    class Meta:
+        """
+        Display profiles in order they were created.
+        Newest first.
+        """
+        ordering = ['-created_on']
+
+    def __str__(self):
+        """
+        Changing display name from ID
+        to username.
+        """
+        return f"{self.username}'s profile."
+
+
+# Function provided by DRF-API walkthrough.
+def create_profile(sender, instance, created, **kwargs):
+    """
+    Function to initiate the creation of a user profile,
+    upon the creation of a new user.
+    """
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
