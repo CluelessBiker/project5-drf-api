3aeae8dce05d17e04afeb2691470b5845ac9d826
Lauren-Nicole <popich.red@gmail.com>
2022-08-09 14:23:27 +0000

    Create view for Followers app.

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from p5_drf_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
 
-# Create your views here.
+
+# Class provided by DRF-API walkthrough.
+class FollowerList(generics.ListCreateAPIView):
+    """
+    Allow user to follow another user.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
+
+    def perform_create(self, serializer):
+        """
+        If a user is authenticated,
+        their follow request shall be
+        saved to the database
+        """
+        serializer.save(owner=self.request.user)
+
+
+# Class provided by DRF-API walkthrough.
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    """
+    Allow a user to follow/unfollow another user.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
