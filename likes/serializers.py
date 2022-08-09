1059378c6f140a9c906f0293fb47eadac5eba6dc
Lauren-Nicole <popich.red@gmail.com>
2022-08-09 13:32:44 +0000

    Create serialzer for Likes app.

---
+++ likes/serializers.py
@@ -0,0 +1,33 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from likes.models import Like
+
+
+# Class provided by DRF-API walkthrough.
+class LikeSerializer(serializers.ModelSerializer):
+    """
+    Likes model serializer.
+    """
+    owner = serializers.ReadOnlyField(soure='owner.username')
+
+    class Meta:
+        model = Like
+        fields = [
+            'id',
+            'created_on',
+            'owner',
+            'post',
+        ]
+
+    def create(self, validate_data):
+        """
+        Supplies an error message should
+        the user attempt to like the same
+        post more than once.
+        """
+        try:
+            return super().create(validate_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
