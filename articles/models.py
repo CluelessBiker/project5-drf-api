5ad422b2ae02e7c9803299e5670b2e258f1de2af
Lauren-Nicole <popich.red@gmail.com>
2022-08-10 10:17:58 +0000

    Create Articles model.

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Article(models.Model):
+    """
+    Articles model.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=300)
+    content = models.TextField(blank=False)
+    image = models.ImageField(
+        upload_to='images/',
+        default='../pexels-artem-podrez-7048043_jblpym',
+        blank=True
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """
+        Order articles by date created.
+        Display by most recent first.
+        """
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f'{self.title}'
