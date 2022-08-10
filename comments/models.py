3888333d02028200350297d1ab840867011a790b
Lauren-Nicole <popich.red@gmail.com>
2022-08-10 07:31:17 +0000

    Create model for Comments app. Migrate database.

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
 
-# Create your models here.
+
+# Class provided by DRF-API walkthrough.
+class Comment(models.Model):
+    """
+    Comments Model.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    class Meta:
+        """
+        Display comments in order of posting.
+        Newest first.
+        """
+        ordering = ['-created_on']
+
+    def __str__(self):
+        """
+        Display content instead of ID.
+        """
+        return self.content
