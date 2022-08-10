b2e2f70da206d9afe29a4b59dea736e7fe1d887d
Lauren-Nicole <popich.red@gmail.com>
2022-08-10 06:58:50 +0000

    Create views for Posts app. Implement function for create & retrieve.

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from p5_drf_api.permissions import IsOwnerOrReadOnly
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+# Class provided by DRF-API walkthrough.
+class PostList(generics.ListCreateAPIView):
+    """
+    Retrieve & Create new posts.
+    """
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    queryset = Post.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_on')
+
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'likes__owner__profile',
+        'owner__profile',
+    ]
+
+    search_fields = [
+        'owner__username',
+        'title',
+    ]
+
+    ordering_fields = [
+        'likes_count',
+        'comments_count',
+        'likes__created_on',
+    ]
+
+    def perform_create(self, serializer):
+        """
+        Check user logged in & authenticated.
+        If so, save new post to database.
+        """
+        serializer.save(owner=self.request.user)
