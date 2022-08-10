459cd93a29357c2649a9ad036ad3d5488f127282
Lauren-Nicole <popich.red@gmail.com>
2022-08-10 12:11:39 +0000

    Resolve PEP8 errors in project settings. Exception of AUTH_PASSWORD_VALIDATORS.

--- p5_drf_api/settings.py
+++ p5_drf_api/settings.py
@@ -78,7 +78,9 @@ if 'CLIENT_ORIGIN' in os.environ:
     ]
 
 if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    extracted_url = re.match(
+        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
+    ).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
