bf53efdc9ea5da7d3a44adc5eeb1ebb48cd25465
Lauren-Nicole <popich.red@gmail.com>
2022-08-10 07:05:05 +0000

    Add function to retrieve, update & delete post.

--- posts/views.py
+++ posts/views.py
@@ -48,3 +48,18 @@ class PostList(generics.ListCreateAPIView):
         If so, save new post to database.
         """
         serializer.save(owner=self.request.user)
+
+
+# Class provided by DRF-API walkthrough.
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Function to Retrieve,
+    Edit, Delete post if owner.
+    """
+    serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    queryset = Post.objects.annotate(
+        likes_count=Count('likes', distinct=True)
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_on')
