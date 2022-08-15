2140c22763ba36297c962a0d328704a0ec0863ce
Lauren-Nicole <popich.red@gmail.com>
2022-08-15 13:49:52 +0000

    Bug issue in Posts app. Test commit.

--- p5_drf_api/settings.py
+++ p5_drf_api/settings.py
@@ -64,8 +64,8 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = 'DEV' in os.environ
+DEBUG = True
+# DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
