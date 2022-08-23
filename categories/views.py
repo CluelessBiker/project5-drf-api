be5a2d4283b2b51169cc3483dc0615435cdf113c
Lauren-Nicole <popich.red@gmail.com>
2022-08-23 10:23:28 +0000

    Create views for categories.

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from categories.models import Category
+from categories.serializers import CategorySerializer
 
-# Create your views here.
+
+class CategoryList(generices.ListCreateAPIView):
+    """
+    Category list.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class CategoryDetail(generics.RetrieveAPIView):
+    """
+    View category.
+    """
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
