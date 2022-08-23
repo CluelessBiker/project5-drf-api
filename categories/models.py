b1035f42dfaff490cde0d1d6dc269259a6a13d6a
Lauren-Nicole <popich.red@gmail.com>
2022-08-23 09:08:36 +0000

    Create categories model.

--- categories/models.py
+++ categories/models.py
@@ -1,2 +1,33 @@
 from django.db import models
 
+
+class Category(models.Model):
+    """
+    Model to supply categories to Articles app.
+    """
+    category_choices = [
+        ('entertainment', 'Entertainment'),
+        ('events', 'Events'),
+        ('in_depth', 'In Depth'),
+        ('opinion', 'Opinion'),
+        ('news', 'News'),
+    ]
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(
+        max_length=55,
+        choices=category_choices,
+        default='none'
+    )
+
+    class Meta:
+        """
+        Order categories.
+        """
+        ordering = ['category']
+
+    def __str__(self):
+        """
+        Return Category name.
+        """
+        return f'Category: {self.category}'
