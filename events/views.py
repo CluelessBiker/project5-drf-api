8e00579ece1c3da455c02977c0e069fa32c599e4
Lauren-Nicole <popich.red@gmail.com>
2022-08-25 08:40:54 +0000

    Create serializer for events model.

--- events/views.py
+++ events/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from p5_drf_api.permissions import IsOwnerOrReadOnly
+from .models import Event
+from .serializers import EventSerializer
 
-# Create your views here.
+
+class EventList(generics.ListCreateAPIView):
+    """
+    Retrieve events from DB.
+    Create new events.
+    """
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Event.objects.all()
+
+    def perform_create(self, serializer):
+        """
+        Check for user authentication.
+        """
+        serializer.save(owner=self.request.user)
+
+        filter_backends = [
+            filters.SearchFilter,
+        ]
+
+        search_fields = [
+            'owner__username',
+            'title',
+            'city',
+            'country',
+        ]
+
+
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, Update & Destroy events.
+    """
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Event.objects.all()
